2bab62a7a564e16f0584efac02bb5cfd29bfba43
Elizenok <liza_cat@mail.ru>
2024-03-31 03:12:22 +0300

    add

---
+++ src/main/java/org/example/Human.java
@@ -0,0 +1,16 @@
+package org.example;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+
+public class Human {
+    private String firstName;
+    private String lastName;
+}
